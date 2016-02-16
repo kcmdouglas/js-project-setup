a8ef1a82f172440231f353902788d975d9c6c7e7
Kassidy Douglas and Krystan Menne <kcmdouglas@gmail.com>
2016-02-16 08:35:05 -0800

    add jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
+
+gulp.task('jshint', function() {
+  return gulp.src(['js/*.js', 'spec/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
